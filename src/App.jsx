967b77d797c3ade1b93a0bd753aa5e265ad03726
JEdwards <jordanscottedwards@gmail.com>
2024-01-22 14:07:46 -0500

    Remove unneeded comments

--- src/App.jsx
+++ src/App.jsx
@@ -46,12 +46,6 @@ const App = () => {
     name: 'State of a component',
     exercises: 14
   }
-  // const part1 = 'Fundamentals of React'
-  // const exercises1 = 10
-  // const part2 = 'Using props to pass data'
-  // const exercises2 = 7
-  // const part3 = 'State of a component'
-  // const exercises3 = 14
 
   return (
     <div>
@@ -60,9 +54,6 @@ const App = () => {
         part1={part1}
         part2={part2}
         part3={part3}
-        // exercises1={exercises1}
-        // exercises2={exercises2}
-        // exercises3={exercises3} 
         />
       <Total 
         exercises1={part1.exercises}
